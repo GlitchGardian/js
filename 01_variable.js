e418279b24cd93d39e03b7e9863a9b1b98240be1
GlitchGardian <abhisheknbhatt394@gmail.com>
2023-11-27 23:04:23 +0530

    about constants,variable,comments

---
+++ 01_variable.js
@@ -0,0 +1,18 @@
+const accountId= 12345;  // semicolon is not compulsory  
+let accountBalance = 65475645; 
+var Name = "Abhi";
+surname = "bhatt"  // you can also define variable without type datatype
+let accountOpenYear;
+
+console.log("Name");  // if you do quote so the parameter is print 
+console.log(Name);    // if you do not do quote the variable value is print
+
+console.table([accountId,accountBalance,Name,surname,accountOpenYear]);
+
+//console.table is used for print the value of multiple variable
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+and this is multilin comment
+*/
